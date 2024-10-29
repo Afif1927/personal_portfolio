b6f7937b2b956373c312dbb4e0562826c895c07e
Mohammad Afif <afifarfan808@gmail.com>
2024-10-29 21:14:52 +0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/personal_portfolio/",
   plugins: [react()],
+  base: "./", // Ensures Vite uses relative paths in production
 });
